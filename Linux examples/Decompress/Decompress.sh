38630b9cecb717d37bc1bfda9ae430e446a85dd2
Anthonyg5005 <anthonyxboxreal@gmail.com>
2023-11-15 08:19:19 -0600

    add brotli decompression

--- Linux examples/Decompress/Decompress.sh
+++ Linux examples/Decompress/Decompress.sh
@@ -1,11 +1,14 @@
 #Set the name of the Brotli file here
 #Use \ to define spaces in the name
 #Example: 'My website backup' would be 'My\ website\ backup'
-FileName=Example\ Folder.tar.br
+export FileName=Example\ Folder.tar.br
 
 export BrotliFile=$FileName\.tar.br
 export TarFile=$FileName\.tar
 
 echo "Step one: Decompressing $BrotliFile to Tar archive"
 
+./brotli -d $BrotliFile -o $TarFile
+
+echo "Step two: Extracting files from $TarFile to a directory
 
